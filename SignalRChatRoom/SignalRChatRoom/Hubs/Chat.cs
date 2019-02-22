9cdc55fe939ca88e579d58222e2a8bed63b4bcc6
Jessie Xie Lingyan (CSI Interfusion Inc) <v-lingyx@microsoft.com>
2019-02-22 19:26:59 +0800

    Update the ChatRoom project

---
+++ SignalRChatRoom/SignalRChatRoom/Hubs/Chat.cs
@@ -0,0 +1,20 @@
+using System;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRChatRoom.Hubs
+{
+
+        public class Chat : Hub
+        {
+            public void BroadcastMessage(string name, string message)
+            {
+                Clients.All.SendAsync("broadcastMessage", name, message);
+            }
+
+            public void Echo(string name, string message)
+            {
+                Clients.Client(Context.ConnectionId).SendAsync("echo", name, message + " (echo from server)");
+            }
+        }
+
+}
